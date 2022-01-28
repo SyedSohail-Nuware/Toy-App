04999e74f74c89c2aa1570a158a0015859867b61
Syed Sohail-Nuware <syed.sohail@nuware.com>
2022-01-28 18:57:01 +0530

    Completed the toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :name, length: {minimum:1,maximum:50},
+    presence: true
+    validates :email, length: {minimum:1,maximum:100},
+                presence: true
 end
